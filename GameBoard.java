6e826d59b7ff2d4841f73a333792ed21871d5bc4
tstieven <thomas.stievenard@orange.fr>
2018-02-01 19:22:53 -0500

    add positionChoice and orientationChoice

--- GameBoard.java
+++ GameBoard.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 public class GameBoard {
 
     //min and max values for x and y
-    static private int min = -100;
-    static private int max = 100;
-
-    //static class Triplet{public int x,y; public Bateau boat;};
+    static private int min = -10;
+    static private int max = 10;
+    
+    static Bateau[][] gameBoardArray=  new Bateau[max-min][max-min];
 
     static HashMap<String,Bateau> player1Boat= new HashMap<String,Bateau>();
     static HashMap<String,Bateau> player2Boat= new HashMap<String,Bateau>();
 
     private static String[] boatNameList ={"ContreTorpilleur", "Croiseur", "Porte Avion", "Sous-Marin" , "Torpilleur"};
+    private static String[] orientationList ={"Horizontal", "Vertical"};
 
 
     private static void print(Object a) {
         System.out.println(a);
     }
-
 
     private static int boatChoice(int player,Scanner sc){
         int choiceDone = 0;
@@ -68,22 +68,61 @@ public class GameBoard {
         return nbr;
     }
 
-
-
     private static int positionChoice(String axe ,Scanner sc) {
         int choiceDone = 0;
         int pos = min - 1;
         while (choiceDone == 0) {
             pos = min - 1;
-            boolean nbrNotOk = true;
-            while (nbrNotOk) {
-            }
-
-
+            print("choisissez une valeur pour l'axe " + axe + " entre " + min + " et " + max);
+            print("cette valeur sera la valeur la plus basse du bateau sur l'axe " + axe);
+            pos = sc.nextInt();
+            if ((pos <= max) && (pos >= min)) {
+                int safeChoiceDone = 0;
+                while (safeChoiceDone == 0) {
+                    print("vous avez choisi " + pos + " etes vous sur de votre choix ? (1 pour oui 0 pour non)");
+                    switch (sc.next()) {
+                        case "0":
+                            safeChoiceDone = 1;
+                            break;
+                        case "1":
+                            safeChoiceDone = 1;
+                            choiceDone = 1;
+                            break;
+                    }
+                }
+            }else{print("valeur invalide");}
         }
         return pos;
     }
-    public static Bateau createBoat(int nbr){
+
+    private static int orientationChoice(Scanner sc){
+        int choiceDone = 0;
+        int orientation =  - 1;
+        while (choiceDone == 0) {
+            orientation = - 1;
+            print("choisissez si votre bateau est place a l'horizontal ou a la vertical");
+            print("0 pour horizontal et 1 pour vertical");
+            orientation = sc.nextInt();
+            if ((orientation == max) || (orientation == min)) {
+                int safeChoiceDone = 0;
+                while (safeChoiceDone == 0) {
+                    print("vous avez choisi " + orientationList[orientation] + " etes vous sur de votre choix ? (1 pour oui 0 pour non)");
+                    switch (sc.next()) {
+                        case "0":
+                            safeChoiceDone = 1;
+                            break;
+                        case "1":
+                            safeChoiceDone = 1;
+                            choiceDone = 1;
+                            break;
+                    }
+                }
+            }else{print("valeur invalide");}
+        }
+        return orientation;
+    }
+
+    private static Bateau createBoat(int nbr){
         switch (nbr){
             case 0: return new ContreTorpilleur();
             case 1: return new Croiseur();
@@ -107,6 +146,11 @@ public class GameBoard {
                 if (player==0){
                     player1Boat.put(boatNameList[nbr],createBoat(nbr));
                 }else {  player2Boat.put(boatNameList[nbr],createBoat(nbr)); }
+
+
+                int x = positionChoice("x",sc);
+                int y = positionChoice("y",sc);
+                int orientation = orientationChoice(sc);
                 boatLeft--;
                 player = (player+1)%2;
             }
